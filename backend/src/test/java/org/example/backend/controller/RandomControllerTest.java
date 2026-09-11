3b0e89979585166d1559adca4985bf4f6199b653
jpf-lab <jeanpierre.fuhs@gmail.com>
2026-09-11 11:40:26 +0200

    switch to correct annotation

--- backend/src/test/java/org/example/backend/controller/RandomControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/RandomControllerTest.java
@@ -4,7 +4,8 @@ import org.example.backend.dto.ItemDTO;
 import org.example.backend.service.RandomService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,7 +14,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(RandomController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class RandomControllerTest {
 
     @Autowired
